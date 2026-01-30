4124e1b5b55419444192972818639c5f1b884c09
happyomid <omid.reza.12001@gmail.com>
2026-01-30 21:26:24 +0330

    add web analytics

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
+
 <template>
   <UApp>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+
+    <!-- This is the important part -->
+    <Analytics />
   </UApp>
 </template>
